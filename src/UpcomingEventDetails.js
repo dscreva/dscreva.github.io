204df98289921194db62b8c319e54b9782bff137
Sanjana Shivakumar <sanjanas1k99@gmail.com>
2020-11-26 23:32:08 +0530

    Added New Event

--- src/UpcomingEventDetails.js
+++ src/UpcomingEventDetails.js
@@ -1,11 +1,11 @@
 const events_new = [
     {
-        title: "Learning From Experiences",
-        date: "19th November, 2020",
+        title: "Introduction to Android Study Jam",
+        date: "28th November, 2020",
         type: "Virtual Event",
-        description: "DSC REVA University in association with DSC India Fireside Chat Series brings before you ”Learning From Experience”. Join us for a one-hour session with Siddhant Agarwal from Google Developer relations on 19th November 2020 at 5:00 PM IST.",
-        image_url: "./static/new_events/FirebaseChat.JPG",
-        link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-learning-from-experiences/"
+        description: "This is an event series for people who wish to be an Android Developer or for those who desire to learn something new. With Android Study Jams, you will have the opportunity to learn Kotlin, the most preferred language that is being used to create these Android applications.",
+        image_url: "./static/new_events/AndroidStudyJam.JPG",
+        link: "https://dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-session-0-introduction-to-android-study-jam-1/"
     
     }
 ]
