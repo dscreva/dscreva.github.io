7c8b5b18cf17b95757ebef5683e43ac31a075e65
Sanjana Shivakumar <sanjanas1k99@gmail.com>
2020-12-08 00:02:48 +0530

    Added Upcoming Event

--- src/PastEventsDetails.js
+++ src/PastEventsDetails.js
@@ -2,8 +2,21 @@
 const events = [
     {
 
+        title: "Introduction to Android Study Jam",
+        image_url: "./static/past_events/AndroidStudyJam.jpg",
+        link: "https://dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-session-0-introduction-to-android-study-jam-1/"
+        
+    },
+    {
+
+        title: "DSC REVA - Website Launch",
+        image_url: "./static/past_events/DscWebsiteLaunch.JPG",
+        link: "https://dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-website-inaugration-dsc-reva/"
+        
+    },
+    {
+
         title: "Learning From Experiences",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/FirebaseChat.JPG",
         link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-learning-from-experiences/"
         
@@ -11,7 +24,6 @@ const events = [
     {
 
         title: "REVA HACK 2020 </> | Powered By REVA Univerity",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/RevaHack.JPG",
         link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-reva-hack-2020/"
         
@@ -19,84 +31,72 @@ const events = [
     {
 
         title: "Hands-on Workshop: Flutter | Let's Build Our First App",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/Flutter.JPG",
         link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-reva-hack-2020-flutter-lets-build-our-first-app/"
 
     },
     {
         title: "Hands-on Workshop: Getting Started with Dart",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/Dart.JPG",
         link: '//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-getting-started-with-dart/',
     
     },
     {
         title: "How to make an Elevator Pitch? ",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/EmilyPerry.JPG",
         link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-reva-hack-2020-how-to-make-an-elevator-pitch/"
     
     },
     {
         title: "Importance of Open Source and my contribution in MLH",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/KunalKushwaha.JPG",
         link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-reva-hack-2020-importance-of-open-source-and-my-contribution-in-mlh/"
     
     },
     {
         title: "How to get started with Fluent Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/SingamMohitReddy.JPG",
         link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-reva-hack-2020-demo-on-how-to-get-started-with-fluent-design-and-more-about-the-css-framework/"
     
     },
     {
         title: "Leveraging AI to super-charge your innovations",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/AkashJames.JPG",
         link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-reva-hack-2020-leveraging-ai-to-super-charge-your-innovations/"
     
     },
     {
         title: "Career, Niche Skills and importance of presentation",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/AqibAhmed.png",
         link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-reva-hack-2020-career-niche-skills-and-importance-of-presentation/"
     
     },
     {
         title: "UI/UX for Developers AND No Code: Future of Design and Tech",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/PunitChawla.png",
         link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-reva-hack-2020-uiux-for-developers-and-no-code-future-of-design-and-tech/"
     
     },
     {
         title: "Build a  Cloud-Connected AR/VR App",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/AlonGrinshpoon.JPG",
         link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-build-a-cloud-connected-arvr-app-in-15-minutes-or-less/"
     
     },
     {
         title: "Hactoberfest Introduction - Hack O' Lantern",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/hacktoberfest.JPG",
         link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-hack-o-lantern/"
     
     },
     {
         title: "30 Days of Google Cloud - Onboarding Session",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/googlecloud.JPG",
         link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-30-days-of-google-cloud-cloud-study-jam/"
     
     },
     {
         title: "DSC REVA 2020 - 2021 Info Session",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas lorem quis ante pulvinar, vel mollis velit euismod.",
         image_url: "./static/past_events/info-session.jpeg",
         link: "//dsc.community.dev/events/details/developer-student-clubs-reva-university-presents-info-session-dsc-reva/"
     
